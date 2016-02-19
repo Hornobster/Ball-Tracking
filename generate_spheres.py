a5412c50a4f9b3ddb13048768a5ae8ca4a881af5
Carlo Vespa <vespacarlo@hotmail.it>
2016-02-19 01:46:14 +0100

    Render 10 spheres with random light position.

--- generate_spheres.py
+++ generate_spheres.py
@@ -1,20 +1,22 @@
-#!/usr/local/bin/python2.7
+#!/usr/local/bin/python3
 import bpy
+import random
+import math
 
 # clean scene
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.delete()
 
 # add sphere in the center
-bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=5, location=(0,0,0))
+sphere = bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=5, location=(0,0,0))
 sphere = bpy.data.objects['Icosphere']
 
 # make sphere active and change to smooth shading
 bpy.context.scene.objects.active = sphere
 bpy.ops.object.shade_smooth()
 
-# define origin empty object
-bpy.ops.object.empty_add(location=(0,0,0))
+# add origin empty object
+origin = bpy.ops.object.empty_add(location=(0,0,0))
 origin = bpy.data.objects['Empty']
 
 # make camera active
@@ -32,10 +34,21 @@ camera.constraints['Track To'].up_axis = 'UP_Y'
 camera.location = (2.5,0,0)
 
 # render settings
-bpy.data.scenes['Scene'].render.filepath = '/Users/carlovespa/Documents/Projects/Ball-Tracking/image.png'
-bpy.data.scenes['Scene'].render.resolution_x = 210
-bpy.data.scenes['Scene'].render.resolution_y = 210
-bpy.data.scenes['Scene'].render.resolution_percentage = 100
-bpy.data.scenes['Scene'].render.alpha_mode = 'TRANSPARENT' # credit: http://blender.stackexchange.com/a/1307
+scene = bpy.data.scenes['Scene']
+scene.render.resolution_x = 210
+scene.render.resolution_y = 210
+scene.render.resolution_percentage = 100
+scene.render.alpha_mode = 'TRANSPARENT' # credit: http://blender.stackexchange.com/a/1307
 
-bpy.ops.render.render(write_still = True)
+# get light
+light = bpy.data.objects['Lamp']
+
+for x in range(10):
+    # position light randomly around center
+    light_dist = random.random() * 5 + 5 # light distance in range [5, 10]
+    light.location = ((math.sin(random.random() * 2 * math.pi) * light_dist, \
+                       math.sin(random.random() * 2 * math.pi) * light_dist, \
+                       math.sin(random.random() * 2 * math.pi) * light_dist))
+    
+    scene.render.filepath = './renders/image%d.png' % x
+    bpy.ops.render.render(write_still = True)
