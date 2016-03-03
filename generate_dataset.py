28f1c01325ea4238f052d5b88c927ef51aaa892e
Carlo Vespa <vespacarlo@hotmail.it>
2016-03-03 20:53:52 +0100

    Place spheres randomly in images.

--- generate_dataset.py
+++ generate_dataset.py
@@ -26,11 +26,11 @@ for x in range(10):
         background = Image.open(os.path.join(bkgDir, bkgFilename))
 
         # create new greyscale image
-        result = Image.new('L', (100, 100))
+        result = Image.new('L', (200, 200))
 
         # copy a 100x100px patch from background and paste it into result
         bkgPatch = background.crop((0, 0, 100, 100))
-        result.paste(bkgPatch, (0, 0, 100, 100))
+        result.paste(bkgPatch, (50, 50, 150, 150))
 
         # with probability 50% add a sphere to the image
         if random.random() < 0.5:
@@ -41,11 +41,16 @@ for x in range(10):
             # copy and resize patch from sphere and paste it into result
             spherePatch = sphere.crop((5, 5, 205, 205))
             sphereDiameter = int(random.random() * 90 + 10) # random integer value in [10, 100]
+            sphereCenter = (random.random() * 100 + 50, random.random() * 100 + 50)
             spherePatch = spherePatch.resize((sphereDiameter, sphereDiameter))
+            pasteBox = (int(sphereCenter[0] - math.floor(sphereDiameter / 2.0)), int(sphereCenter[1] - math.floor(sphereDiameter / 2.0)), \
+                        int(sphereCenter[0] + math.ceil(sphereDiameter / 2.0)), int(sphereCenter[1] + math.ceil(sphereDiameter / 2.0))) 
             result.paste(spherePatch, \
-                         (50 - int(math.floor(sphereDiameter / 2.0)), 50 - int(math.floor(sphereDiameter / 2.0)), \
-                         50 + int(math.ceil(sphereDiameter / 2.0)), 50 + int(math.ceil(sphereDiameter / 2.0))), \
+                         pasteBox,
                          spherePatch) # the second spherePatch is used as alpha mask
+
+        # crop to 100x100
+        result = result.crop((50, 50, 150, 150))
 
         # save result
         result.save(os.path.join(datasetDir, 'data%d.png' % x))
